bfa4251722f8baaeed00619d3ad7a2377321f7dd
Deepanshu Saini <sainids785@gmail.com>
2022-10-25 18:11:23 +0530

    added theme, add button and floating button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: missing_required_param, missing_return, unused_element, unused_import, deprecated_member_use
 
-import './widgets/user_transactions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widgets/new_transaction.dart';
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
+import './models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,23 +12,94 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.lime,
+        accentColor: Colors.blue,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+        appBarTheme: AppBarTheme(
+            titleTextStyle: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: 25,
+                fontWeight: FontWeight.w800)),
+      ),
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   // String titleInput;
-  // String amountInput;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+      backgroundColor: Colors.green,
+      elevation: 5,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expense Tracker'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            color: Colors.red,
+            hoverColor: Colors.black,
+            onPressed: () => _startAddNewTransaction(context),
+          ),
+        ],
+        title:
+            Text('Expense Tracker', style: TextStyle(fontFamily: 'Open Sans')),
       ),
       body: Container(
         child: SingleChildScrollView(
@@ -35,15 +110,23 @@ class MyHomePage extends StatelessWidget {
               Container(
                 width: double.infinity,
                 child: Card(
-                  color: Colors.blue,
                   child: Text('CHART!'),
+                  color: Colors.green,
                   elevation: 5,
                 ),
               ),
-              UserTransactions()
+              TransactionList(_userTransactions),
             ],
           ),
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Add'),
+        icon: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
+        backgroundColor: Colors.purpleAccent,
+        elevation: 8,
       ),
     );
   }
